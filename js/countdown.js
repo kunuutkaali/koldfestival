ddca6efc2bbf67a7b3549774250053af6412257d
Munaawad <104926623+Munaawad@users.noreply.github.com>
2022-12-14 09:04:08 +0100

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,3 +1,27 @@
+// have used https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_countdown 12/12/2022
+
 // date counting down to the festival start
-var countDownDate = new Date ("Feb 3, 2023 16:00:00").getTime();
+const countDownDate = new Date ("Feb 3, 2023 16:00:00").getTime();
+
+// countdown time
+var contdownTime = countDownDate - now
+
+// todays day
+var now = new Date().getTime();
+
+// how the does time work
+const Dage = Timer 24;
+const Timer = Minutter * 60;
+const Minutter = Sekunder * 60;
+const Sekunder=  1000;
+
+//Calculate countdown
+const Dage = Math.floor(contdownTime/ dage);
+const Timer = Math.floor(contdownTime %/ Timer);
+const Minutter = Math.floor(contdownTime % /Minutter);
+const Sekunder = Math.floor(contdownTime % /Sekunder);
+
+
+//
+
 
